9c2865a5b2ed489edb99a7734951d78ff3352269
franciskvolquez <franciskvolquez@hotmail.com>
2017-02-11 20:43:39 -0400

    Add albums and reviews controllers

--- MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/Models/Album.cs
@@ -1,11 +1,13 @@
-﻿namespace MvcMusicStore.Models
+﻿using System.Collections.Generic;
+
+namespace MvcMusicStore.Models
 {
     public class Album
     {
         public int AlbumID { get; set; }
         public string Title { get; set; }
         public Artist Artist { get; set; }
-        public Review Review { get; set; }
+        public virtual List<Review> Review { get; set; }
 
     }
 }
